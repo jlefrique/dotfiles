e764dc56ad64823d4d592c8b1eb0f564affd8098
Julien Lefrique <julien.lefrique@gmail.com>
2012-06-08 17:01:31 +0200

    Add Xmonad configuration.

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,29 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+myManageHook = composeAll
+    [ className =? "Gimp"      --> doFloat
+    , className =? "Vncviewer" --> doFloat
+    ]
+
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8"]
+
+main = do
+    xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
+                        <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP $ xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , workspaces = myWorkspaces
+        } `additionalKeys`
+        [ ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
