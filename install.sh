01fb60c7f2675c50bed74a9a0bab4a2dc0e87b0a
Julien Lefrique <julien.lefrique@gmail.com>
2012-12-03 18:01:14 +0100

    Better support of dot-folders in the installation script.

--- install.sh
+++ install.sh
@@ -10,39 +10,53 @@ BASE_PATH="$HOME"
 DOTFILES="$BASE_PATH/dotfiles"
 BIN="$BASE_PATH/bin"
 
-SYMLINKS="\
-vim
-vimrc
-tmux.conf
-xmonad
-dmenurc
-mutt
-muttrc
-zsh
-zshrc
-gitconfig
-xsession
+FILES="\
 Xresources
+dmenurc
+gitconfig
+mailcap
+muttrc
+tmux.conf
+vimrc
+xsession
+zshrc"
+
+FOLDERS="\
+config
+mutt
+xmonad
+vim
 xrdb
-mailcap"
+zsh"
 
 [ -f "$GIT" ] || $(echo "Please install git." && exit 1)
 
 # Clone or update the repository
 if [ -d "$DOTFILES" ]; then
-  pushd "$DOTFILES" > /dev/null
-  $GIT pull --recurse-submodules origin master
-  popd > /dev/null
+    pushd "$DOTFILES" > /dev/null
+    $GIT pull --recurse-submodules origin master
+    popd > /dev/null
 else
-  $GIT clone --recursive "$REPOSITORY"  "$DOTFILES"
+    $GIT clone --recursive "$REPOSITORY" "$DOTFILES"
 fi
 
 $DOTFILES/bin/check_bin.sh
 
-# Create symlinks for dotfiles
-for file in $SYMLINKS; do
-  [ -h "$BASE_PATH/.$file" ] && rm "$BASE_PATH/.$file"
-  ln -s "$DOTFILES/$file" "$BASE_PATH/.$file"
+# Create symlinks for files
+for file in $FILES; do
+    [ -h "$BASE_PATH/.$file" ] && rm "$BASE_PATH/.$file"
+    ln -s "$DOTFILES/$file" "$BASE_PATH/.$file"
+done
+
+# Create symlinks for the files in the different folders
+for folder in $FOLDERS; do
+    # Create the folder if it doesn't exist
+    [ -d "$BASE_PATH/.$folder" ] || mkdir "$BASE_PATH/.$folder"
+
+    for file in `ls "$DOTFILES/$folder"`; do
+        [ -h "$BASE_PATH/.$folder/$file" ] && rm "$BASE_PATH/.$folder/$file"
+        ln -s "$DOTFILES/$folder/$file" "$BASE_PATH/.$folder/$file"
+    done
 done
 
 # Create symlinks for binaries
