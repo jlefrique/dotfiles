512de5a12e300e162d67ae4e593afe35942486fd
Julien Lefrique <julien.lefrique@gmail.com>
2013-04-25 09:47:21 +0200

    Adjust PYTHONPATH.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,4 +1,4 @@
 # Path
 export PATH=${PATH}:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools
 export PATH=${PATH}:~/JP/tools:~/JP/tools/debpath:~/JP/tools/visualization:~/JP/tools/PaverXmlRpc
-export PYTHONPATH=$PYTHONPATH:~/JP/tools/PaverXmlRpc
+export PYTHONPATH=$PYTHONPATH:~/JP/tools/BuildRpc
