6722729e2ae691d1c702a2792ef24fec62f53846
Julien Lefrique <julien.lefrique@gmail.com>
2013-05-15 13:19:47 +0200

    Add git-annex to the PATH.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,4 +1,4 @@
 # Path
-export PATH=${PATH}:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools
-export PATH=${PATH}:~/JP/tools:~/JP/tools/debpath:~/JP/tools/visualization:~/JP/tools/PaverXmlRpc
+export PATH=${PATH}:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools:~/git-annex
+export PATH=${PATH}:~/JP/tools:~/JP/tools/debpath:~/JP/tools/visualization:~/JP/tools/BuildRpc
 export PYTHONPATH=$PYTHONPATH:~/JP/tools/BuildRpc
