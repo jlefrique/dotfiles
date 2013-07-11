8c05f319edc09cb77ae025e72684ccc2a5ec3624
Julien Lefrique <julien.lefrique@gmail.com>
2013-07-11 13:27:19 +0200

    Create a script to launch trayer.

---
+++ bin/tray.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+trayer --edge top --align right --SetDockType true --expand true \
+    --widthtype percent --width 10 --transparent true \
+    --alpha 0 --tint 0x000000 --heighttype pixel --height 18 &
