67d02f547417ac00973813fd7cd65a46a085d667
Julien Lefrique <julien.lefrique@gmail.com>
2013-08-19 10:04:10 +0200

    Change directory with ranger.

---
+++ zsh/ranger.zsh
@@ -0,0 +1,11 @@
+# Automatically change the directory after closing ranger
+
+rcd () {
+    tempfile='/tmp/chosendir'
+    ranger --choosedir="$tempfile" "${@:-$(pwd)}"
+    test -f "$tempfile" &&
+    if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
+        cd -- "$(cat "$tempfile")"
+    fi
+    rm -f -- "$tempfile"
+}
