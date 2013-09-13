1e3598301cc1d499d480f1572b2d86261bd3204a
Julien Lefrique <julien.lefrique@gmail.com>
2013-09-13 12:19:48 +0200

    Autocomplete with files for the second argument.

--- zsh/autocomplete.zsh
+++ zsh/autocomplete.zsh
@@ -1,7 +1,10 @@
 # Autocomplete for paver.
-_paver() {
-    TASKS=`paver -hq | awk '/^  ([a-zA-Z][a-zA-Z0-9_]+)/ {print $1}'`
-    _arguments "1: :(${TASKS})"
+_paver () {
+    declare tasks
+
+    tasks=$(paver -hq | awk '/^  ([a-zA-Z][a-zA-Z0-9_]+)/ {print $1}')
+    _arguments "1: :(${tasks})" \
+               "2: :_files"
 }
 
 compdef _paver paver
