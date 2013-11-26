a8204a9bf1e9e8cef7f748006b8e34e0a1c8eeb2
Julien Lefrique <julien.lefrique@gmail.com>
2013-11-26 18:43:53 +0100

    Add screen's configuration for walnut.

--- bin/set_screen.sh
+++ bin/set_screen.sh
@@ -2,9 +2,12 @@
 
 case "$(hostname)" in
 
+    walnut) \
+        xrandr --output LVDS --primary --mode 1400x1050 --rotate normal ;;
+
     aspen) \
-        xrandr --output "DFP1" --primary --mode 1680x1050 --rotate normal && \
-        xrandr --output "CRT1" --mode 1920x1080 --right-of "DFP1" --rotate normal ;;
+        xrandr --output DFP1 --primary --mode 1680x1050 --rotate normal && \
+        xrandr --output CRT1 --mode 1920x1080 --right-of DFP1 --rotate normal ;;
 
     *) echo "No configuration found." ;;
 
