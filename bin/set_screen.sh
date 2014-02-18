d48bdb67d563c9df468c03e7f8432c6d15fb4d4b
Julien Lefrique <julien.lefrique@gmail.com>
2014-02-18 11:30:24 +0100

    The names of the monitors have changed.

--- bin/set_screen.sh
+++ bin/set_screen.sh
@@ -6,8 +6,8 @@ case "$(hostname)" in
         xrandr --output LVDS --primary --mode 1400x1050 --rotate normal ;;
 
     aspen) \
-        xrandr --output DFP1 --primary --mode 1680x1050 --rotate normal && \
-        xrandr --output CRT1 --mode 1920x1080 --right-of DFP1 --rotate normal ;;
+        xrandr --output DVI-0 --primary --mode 1680x1050 --rotate normal && \
+        xrandr --output VGA-0 --mode 1920x1080 --right-of DVI-0 --rotate normal ;;
 
     *) echo "No configuration found." ;;
 
