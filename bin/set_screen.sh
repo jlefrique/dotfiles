ffbaefbeaae602dda5f3e652186355d67db8b48b
Julien Lefrique <julien.lefrique@gmail.com>
2014-04-18 18:28:45 +0200

    Handle TV output.

--- bin/set_screen.sh
+++ bin/set_screen.sh
@@ -1,14 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 case "$(hostname)" in
 
-    walnut) \
-        xrandr --output LVDS --primary --mode 1400x1050 --rotate normal ;;
+    walnut)
+        xrandr --output LVDS --primary --mode 1400x1050 --rotate normal
+        if [[ "$1" == "tv" ]]; then
+            xrandr --output VGA-0 --mode 800x600 --left-of LVDS
+        fi
+    ;;
 
-    aspen) \
-        xrandr --output DVI-0 --primary --mode 1680x1050 --rotate normal && \
-        xrandr --output VGA-0 --mode 1920x1080 --right-of DVI-0 --rotate normal ;;
+    aspen)
+        xrandr --output DVI-0 --primary --mode 1680x1050 --rotate normal
+        xrandr --output VGA-0 --mode 1920x1080 --right-of DVI-0 --rotate normal
+    ;;
 
-    *) echo "No configuration found." ;;
+    *) echo "No screen configuration found." ;;
 
 esac
