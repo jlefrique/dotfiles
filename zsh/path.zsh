de13f78616f7db72ba3e9002b5e34592f93482a3
Julien Lefrique <julien.lefrique@gmail.com>
2014-05-10 19:40:52 +0200

    Clean $PATH and $PYTHONPATH.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,4 +1,3 @@
 # Path
+
 export PATH=${PATH}:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools:~/git-annex
-export PATH=${PATH}:~/JP/tools:~/JP/tools/debpath:~/JP/tools/visualization:~/JP/tools/BuildRpc
-export PYTHONPATH=$PYTHONPATH:~/JP/tools/BuildRpc:~/JP/libs/jsonrpc-python
