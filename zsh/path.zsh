169161751dc411a82a588b2d99c21b84c9303231
Julien Lefrique <julien.lefrique@gmail.com>
2014-07-17 12:45:58 +0200

    Add Android NDK to the path.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,3 @@
 # Path
 
-export PATH=${PATH}:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools:~/git-annex
+export PATH=${PATH}:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools:~/android-ndk:~/git-annex
