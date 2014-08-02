f14856e697428929dae5315062465939c0732554
Julien Lefrique <julien.lefrique@gmail.com>
2014-08-02 18:22:01 +0200

    Add support for external monitor.

--- bin/set_screen.sh
+++ bin/set_screen.sh
@@ -3,9 +3,22 @@
 case "$(hostname)" in
 
     walnut)
-        xrandr --output LVDS --primary --mode 1400x1050 --rotate normal
-        if [[ "$1" == "tv" ]]; then
-            xrandr --output VGA-0 --mode 800x600 --left-of LVDS
+
+        # Laptop screen
+        WALNUT_INT=LVDS
+        WALNUT_INT_MODE=1400x1050
+
+        # External screen
+        WALNUT_EXT=VGA-0
+        WALNUT_EXT_MODE=1920x1200
+
+        if [[ $(xrandr | grep -c "$WALNUT_EXT connected") -eq 0 ]]
+        then
+            xrandr --output $WALNUT_INT --mode $WALNUT_INT_MODE --rotate normal --primary
+        else
+            echo "External monitor connected."
+            xrandr --output $WALNUT_EXT --mode $WALNUT_EXT_MODE --rotate normal --primary
+            xrandr --output $WALNUT_INT --mode $WALNUT_INT_MODE --rotate normal --right-of $WALNUT_EXT
         fi
     ;;
 
