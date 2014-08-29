92469716651a01886d8a93401f409ae6b5337fd4
Julien Lefrique <julien.lefrique@gmail.com>
2014-08-29 12:09:26 +0200

    Add the Tizen SDK to the path.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,5 @@
 # Path
 
-export PATH=${PATH}:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools:~/android-ndk:~/git-annex
+export PATH=${PATH}:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools:~/android-ndk
+export PATH=${PATH}:~/tizen-sdk/tools
+export PATH=${PATH}:~/git-annex
