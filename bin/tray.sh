d9253d10c3272018ffb5f1ee024eb5a451238a9e
Julien Lefrique <julien.lefrique@gmail.com>
2014-11-30 11:49:47 +0100

    Reduce the size of xmobar and trayer.

--- bin/tray.sh
+++ bin/tray.sh
@@ -2,4 +2,4 @@
 
 trayer --edge top --align right --SetDockType true --expand true \
     --widthtype percent --width 10 --transparent true \
-    --alpha 0 --tint 0x000000 --heighttype pixel --height 18 &
+    --alpha 0 --tint 0x000000 --heighttype pixel --height 16 &
